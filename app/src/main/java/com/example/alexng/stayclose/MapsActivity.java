ccf6c85eea2c1aa6f9e427b0fcadcb17774b08c4
Eric K.W. Kim <erickimkw@gmail.com>
2017-08-12 14:20:24 -0400

    init test

--- app/src/main/java/com/example/alexng/stayclose/MapsActivity.java
+++ app/src/main/java/com/example/alexng/stayclose/MapsActivity.java
@@ -9,7 +9,7 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
-// hello alex this is a test
+// hello alex this is a test!
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
