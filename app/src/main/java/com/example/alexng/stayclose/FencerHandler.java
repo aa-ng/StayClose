c365528f0c724f31b8b5701da0c44206079f3493
Ming <mingyuanli.5000@gmail.com>
2017-08-12 18:32:34 -0400

    Added Fence Registration

--- app/src/main/java/com/example/alexng/stayclose/FencerHandler.java
+++ app/src/main/java/com/example/alexng/stayclose/FencerHandler.java
@@ -1,18 +1,29 @@
 package com.example.alexng.stayclose;
 
+import android.*;
+import android.Manifest;
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.text.TextUtils;
+import android.util.Log;
 
 import com.google.android.gms.awareness.Awareness;
+import com.google.android.gms.awareness.fence.AwarenessFence;
 import com.google.android.gms.awareness.fence.FenceState;
+import com.google.android.gms.awareness.fence.FenceUpdateRequest;
+import com.google.android.gms.awareness.fence.LocationFence;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
 
 /**
  * Created by Ming on 2017-08-12.
@@ -28,7 +39,7 @@ public class FencerHandler {
 	private final String FENCE_KEY = "fence_key";
 	private GoogleApiClient mApiClient;
 
-	public FencerHandler(final FragmentActivity context){
+	public FencerHandler(final FragmentActivity context, final double latitude, final double longitutde, final double radius) {
 		mApiClient = new GoogleApiClient.Builder(context)
 				.addApi(Awareness.API)
 				.enableAutoManage(context, 1, null)
@@ -43,7 +54,7 @@ public class FencerHandler {
 						// The broadcast receiver that will receive intents when a fence is triggered.
 						mFenceReceiver = new FenceReceiver();
 						context.registerReceiver(mFenceReceiver, new IntentFilter(FENCE_RECEIVER_ACTION));
-						//setupFences();
+						setupFences(context, latitude, longitutde, radius);
 					}
 
 					@Override
@@ -52,6 +63,40 @@ public class FencerHandler {
 				})
 				.build();
 
+	}
+
+	private void setupFences(FragmentActivity context, double latitude, double longitude, double radius) {
+
+		if (ActivityCompat.checkSelfPermission(context, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+			// TODO: Consider calling
+			//    ActivityCompat#requestPermissions
+			// here to request the missing permissions, and then overriding
+			//   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+			//                                          int[] grantResults)
+			// to handle the case where the user grants the permission. See the documentation
+			// for ActivityCompat#requestPermissions for more details.
+			ActivityCompat.requestPermissions(context, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 0);
+			//Log.e(getClass().toString(), "Permission denied");
+			//return;
+		}
+		AwarenessFence exitingFence = LocationFence.exiting(latitude, longitude, radius);
+
+		// Register the fence to receive callbacks.
+		Awareness.FenceApi.updateFences(
+				mApiClient,
+				new FenceUpdateRequest.Builder()
+						.addFence(FENCE_KEY, exitingFence, mPendingIntent)
+						.build())
+				.setResultCallback(new ResultCallback<Status>() {
+					@Override
+					public void onResult(@NonNull Status status) {
+						if(status.isSuccess()) {
+							Log.i(getClass().toString(), "Fence was successfully registered.");
+						} else {
+							Log.e(getClass().toString(), "Fence could not be registered: " + status);
+						}
+					}
+				});
 	}
 
 	/**
@@ -76,8 +121,10 @@ public class FencerHandler {
 				switch (fenceState.getCurrentState()) {
 					case FenceState.TRUE:
 						fenceStateStr = "true";
+						Log.i(getClass().toString(), "Fence State = true");
 						break;
 					case FenceState.FALSE:
+						Log.i(getClass().toString(), "Fence State = false");
 						fenceStateStr = "false";
 						break;
 					case FenceState.UNKNOWN:
